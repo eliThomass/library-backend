c35e55a6a4fc1d3990977e0ff04dbc4d173f4e3c
burhantaskin <112017727+burhantaskin@users.noreply.github.com>
2026-04-21 12:24:42 -0700

    BookRepository.cs

---
+++ LibraryAPI/Repositories/BookRepository.cs
@@ -0,0 +1,54 @@
+using LibraryBookBorrowingSystem.Data;
+using LibraryBookBorrowingSystem.Models;
+using LibraryBookBorrowingSystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryBookBorrowingSystem.Repositories;
+
+public class BookRepository : IBookRepository
+{
+    private readonly AppDbContext _context;
+
+    public BookRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Book>> GetAllAsync()
+    {
+        return await _context.Books
+            .AsNoTracking()
+            .OrderBy(b => b.Id)
+            .ToListAsync();
+    }
+
+    public async Task<Book?> GetByIdAsync(int id)
+    {
+        return await _context.Books
+            .FirstOrDefaultAsync(b => b.Id == id);
+    }
+
+    public async Task<Book?> GetByIsbnAsync(string isbn)
+    {
+        return await _context.Books
+            .FirstOrDefaultAsync(b => b.ISBN == isbn);
+    }
+
+    public async Task AddAsync(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Book book)
+    {
+        _context.Books.Update(book);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Book book)
+    {
+        _context.Books.Remove(book);
+        await _context.SaveChangesAsync();
+    }
+}
