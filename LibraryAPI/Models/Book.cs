6b4c102d7dff0a994e84448c66945846f836fd2e
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-04-21 19:24:01 -0700

    Update Book.cs

--- LibraryAPI/Models/Book.cs
+++ LibraryAPI/Models/Book.cs
@@ -19,16 +19,20 @@ public class Book
 =======
 
     [Required]
+    [MaxLenght(100)]
     public string Title { get; set; } = string.Empty;
 
     [Required]
+    [MaxLenght(20)]
     public string Author { get; set; } = string.Empty;
 
     [Required]
+    [MaxLenght(20)]
     public string ISBN { get; set; } = string.Empty;
-
+    [Range(0, int.MaxValue)]
     public int TotalCopies { get; set; }
 
+    [Range(0, int.MaxValue)]
     public int AvailableCopies { get; set; }
 }
 >>>>>>> main
